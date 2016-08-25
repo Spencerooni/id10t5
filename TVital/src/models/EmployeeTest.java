619ebec7eee4e4649c1a8ab7783e62ca703e35cc
Harry Henry <h.henry@kainos.com>
2016-08-25 10:45:05 +0100

    Employee Pojo test created

---
+++ TVital/src/models/EmployeeTest.java
@@ -0,0 +1,27 @@
+package models;
+
+public class EmployeeTest {
+		private String firstName;
+		private String lastName;
+		
+	public EmployeeTest() {
+		//Empty Constructor
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+		
+}
