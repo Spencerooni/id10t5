3069c6e58571dff61d9f7dc3497506a5cee7ce31
Harry Henry <h.henry@kainos.com>
2016-08-25 11:41:53 +0100

    Additional POJOs added for final use - updated spike POJO

---
+++ TVital/src/models/EmployeeTester.java
@@ -0,0 +1,27 @@
+package models;
+
+public class EmployeeTester {
+	private String firstName;
+	private String lastName;
+
+	public EmployeeTester() {
+		//Empty Constructor
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
