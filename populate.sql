c8849c184fd0cd7a19d908f3bb56253340923116
Michael Hall <m.hall@kainos.com>
2016-08-25 12:59:15 +0100

    script for populating db

---
+++ populate.sql
@@ -0,0 +1,15 @@
+
+insert into testdb
+values (1,"Harry","Henry");
+
+insert into testdb
+values (2,"Michale","Hall");
+
+insert into testdb
+values (3,"Daniel","Friend");
+
+insert into testdb
+values (4,"Ryan","Fay");
+
+insert into testdb
+values (5,"Bob","Smith");
